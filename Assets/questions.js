16e6aad0e36f0a53028e72719da69d9c1df76dbd
luismiguelgp3 <luismiguelgoncalves172@gmail.com>
2025-06-06 21:40:47 -0300

    Criando perguntas para o quiz

---
+++ Assets/questions.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    question: "Qual é o principal objetivo do AtmoGuard?",
+    answers: [
+      { option: "Criar previsões climáticas semanais", correct: false },
+      { option: "Monitorar desastres naturais em tempo real", correct: true },
+      { option: " Fornecer notícias sobre o clima", correct: false },
+    ],
+  },
+  {
+    question: "Qual evento natural teve aumento de 200% no Brasil desde 2019?Quanto é 5 x 2?",
+    answers: [
+      { option: "Terremotos", correct: false },
+      { option: "Enchentes ", correct: true },
+      { option: "Nevascas", correct: false },
+    ],
+  },
+  {
+    question: "Quantos metros é o raio de mapeamento de alertas do AtmoGuard?",
+    answers: [
+      { option: "1000 metros", correct: false },
+      { option: "3000 metros", correct: false },
+      { option: " 5000 metros", correct: true },
+    ],
+  },
+  {
+    question: "Quais dados o AtmoGuard usa para notificar os usuários?",
+    answers: [
+      { option: "Dados aleatórios da internet", correct: false },
+      { option: "Resultados de enquetes públicas", correct: false },
+      { option: "Dados em tempo real via API", correct: true },
+    ],
+  },
+ {
+    question: "Que tipo de impacto o AtmoGuard busca reduzir?",
+    answers: [
+      { option: "Financeiro apenas", correct: false },
+      { option: "Ambiental e artístico", correct: true },
+      { option: " Social, econômico e de saúde ", correct: false },
+    ],
+  },
+
+  {
+    question: "Quem pode se beneficiar com o AtmoGuard?",
+    answers: [
+      { option: "Apenas moradores de áreas urbanas", correct: false },
+      { option: "Qualquer pessoa, independentemente da localização ", correct: false },
+      { option: "Somente quem tem plano premium", correct: true },
+    ],
+  },
